f8b40773230bf005cf6f906f51cbca09804c70d4
conig <james.conigrave@gmail.com>
2025-05-06 10:30:27 +1000

    add test for nbinom2 and clean up empty lines

--- tests/testthat/test-glmmTMB.R
+++ tests/testthat/test-glmmTMB.R
@@ -1,28 +1,26 @@
 library(glmmTMB)
 
 test_that("Poisson glmmTMB succeeds", {
-
-
   m1 <- glmmTMB(y ~ Ed + (1|So), data = MASS::UScrime, family = "poisson")
   m2 <- glmmTMB(y ~ Ed + I(M.F/1000) + (1|So), data = MASS::UScrime, family = "poisson")
-
   testthat::expect_no_error(mtab::h_tab(m1, m2))
-
-
 })
 
 test_that("Logistic glmmTMB succeeds", {
 
   m1 <- glmmTMB(incidence > 0 ~ size + (1|herd), data = lme4::cbpp, family = "binomial")
   m2 <- glmmTMB(incidence > 0 ~ size + period + (1|herd), data = lme4::cbpp, family = "binomial")
-
   testthat::expect_no_error(mtab::h_tab(m1, m2))
-
 })
 
-test_that("nbinom1 glmmTMB successed",{
+test_that("nbinom1 glmmTMB succeeds",{
   m1 <- glmmTMB(y ~ Ed + (1|So), data = MASS::UScrime, family = nbinom1())
   m2 <- glmmTMB(y ~ Ed + I(M.F/1000) + (1|So), data = MASS::UScrime, family = nbinom1())
+  testthat::expect_no_error(mtab::h_tab(m1, m2))
+})
 
+test_that("nbinom2 glmmTMB succeeds",{
+  m1 <- glmmTMB(y ~ Ed + (1|So), data = MASS::UScrime, family = nbinom2())
+  m2 <- glmmTMB(y ~ Ed + I(M.F/1000) + (1|So), data = MASS::UScrime, family = nbinom2())
   testthat::expect_no_error(mtab::h_tab(m1, m2))
 })
