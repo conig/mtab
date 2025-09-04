d689dc74f0aa5734d0a51b350a22c5ec337f8145
conig <jamesconigrave@gmail.com>
2025-09-04 13:26:53 +1000

    make safe converge work silently.

--- R/model_utils.R
+++ R/model_utils.R
@@ -1,5 +1,17 @@
 safe_convergence <- function(m) {
-  conv <- try(performance::check_convergence(m), silent = TRUE)
+  # Suppress warnings, messages, and any printed output from upstream
+  conv <- NULL
+  tmp <- utils::capture.output({
+    conv <- try(
+      suppressWarnings(
+        suppressMessages(
+          performance::check_convergence(m)
+        )
+      ),
+      silent = TRUE
+    )
+  })
+
   if (is.null(conv)) {
     return(NA)
   }
